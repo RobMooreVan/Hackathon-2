e52283e2ca4272418ebf86f7878bfd438d1621a5
RobMooreVan <124092134+RobMooreVan@users.noreply.github.com>
2023-03-30 14:16:45 -0700

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.scss";
+// import logo from "../../assets/logo/InStock-Logo_1x.png";
+import { useLocation, Link } from "react-router-dom";
+
+import "./Header"
+
+function Header () {
+  return(
+    <div>
+      <h1>Header yo!</h1>
+    </div>
+  );
+}
+
+export default Header
