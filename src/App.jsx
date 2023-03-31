d3fae6f232955a5507efaed828b9e6de1196a893
jvpoly <54869526+jvpoly@users.noreply.github.com>
2023-03-31 12:31:54 -0700

    links added to navbar and dynamic chat component finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,46 @@ import TwoRetentionPage from "./pages/TwoRetentionPage/TwoRetentionPage";
 import AdvancePage from "./pages/AdvancePage/AdvancePage";
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
+import { useState } from "react";
 
 function App() {
+  const [chatState, setChatState] = useState("");
+
+  const handleChatContent = (chatState) => {
+    setChatState(chatState);
+  };
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/retention" element={<RetentionPage />} />
-        <Route path="/tworetention" element={<TwoRetentionPage />} />
+        <Route
+          path="/main"
+          element={
+            <MainPage
+              chatState={chatState}
+              handleChatContent={handleChatContent}
+            />
+          }
+        />
+        <Route
+          path="/retention"
+          element={
+            <RetentionPage
+              chatState={chatState}
+              handleChatContent={handleChatContent}
+            />
+          }
+        />
+        <Route
+          path="/tworetention"
+          element={
+            <TwoRetentionPage
+              chatState={chatState}
+              handleChatContent={handleChatContent}
+            />
+          }
+        />
         <Route path="/advance" element={<AdvancePage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/RetentionPage/RetentionPage.jsx
+++ src/pages/RetentionPage/RetentionPage.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import Chat from "../../components/Chat/Chat";
 
-function RetentionPage() {
+function RetentionPage({ chatState, handleChatContent }) {
+  useEffect(() => {
+    handleChatContent("retention");
+  }, []);
   return (
     <>
-    <div className='page-layout'> {/* do not touch pls */}
-
-    <div>RetentionPage</div>
-    <Link to="/main">Link to main page</Link>
-    </div>
+      <div className="page-layout">
+        {" "}
+        {/* do not touch pls */}
+        <div>RetentionPage</div>
+        <Chat chatState={chatState} />
+        <Link to="/main">Link to main page</Link>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default RetentionPage
+export default RetentionPage;
