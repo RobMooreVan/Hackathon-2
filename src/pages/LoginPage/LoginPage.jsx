672d2a44f93fa7d5239e80c3a2df91cfbe9d1440
RobMooreVan <124092134+RobMooreVan@users.noreply.github.com>
2023-03-31 12:55:57 -0700

    edited login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,19 +3,24 @@ import { Link } from "react-router-dom";
 
 // import axios from "axios";
 import "./LoginPage.scss";
+import vLogo from "../../assets/logo/Logo-remade.png"
 
 // import Header from "../../components/Header/Header";
 
 function LoginPage() {
   return(
     <div className="page-layout">  {/* do not touch pls */}
-      <h1>Login Page</h1>
-      <div className="login-container">
-      <label>Username</label>
-      <input type="" />     
-      <label>Password</label>
-      <input type="" />   
-      <Link to="/main">Login</Link>  
+      <div className="login-box">
+        <div className="top">
+          <img src={vLogo} alt="logo"/>
+        </div>
+        <div className="login-container">
+        <label>Username</label>
+        <input className="login-input " type="input" />     
+        <label className="pw">Password</label>
+        <input className="login-input" type="password" />   
+        </div>
+        <Link className="login__btn" to="/main">Login</Link>  
       </div>
     </div>
   );
